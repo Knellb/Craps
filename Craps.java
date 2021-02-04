510a2d33cd2e5957e72b80bcf90bdb8cd2bf6128
Ben Knell <bknell21@students.polytechnic.org>
2021-02-04 13:55:41 -0800

    Craps program partly done

--- Craps.java
+++ Craps.java
@@ -23,9 +23,44 @@ public class Craps
             System.out.println("On this first roll, a 7 or an 11 automatically wins, and a 2, 3, or 12 automatically loses, and play is over. If a 4, 5, 6, 8, 9, or 10 are rolled on this first roll, that number becomes the 'point.'");
             System.out.println("You continue to roll the two dice again until one of two things happens: either you roll the 'point' from that first roll again, in which case you win; or you roll a 7, in which case you lose.");
         }
+        Die d1 = new Die();
+        Die d2 = new Die();
+        int total = d1.roll() + d2.roll();
+        boolean didTheyWin = true;
         
-        
+        if (total == 7 || total == 11)
+        {
+            didTheyWin = true;
+        }
+        else if (total == 2 || total == 3 || total == 12)
+        {
+            didTheyWin = false;
+        }
+        else
+        {
+            int winPoint = total;
+            int lossPoint = 7;
+            while (total != lossPoint && total != winPoint)
+            {
+                total = d1.roll() + d2.roll();
+                System.out.println("You rolled a " + total);
+            }
+            if (total == winPoint)
+            {
+                didTheyWin = true;
+            }
+            else
+            {
+                didTheyWin = false;
+            }
+        }
+        if (didTheyWin == true)
+        {
+            System.out.println("Congratulations! You won! Play again?");
+        }
+        else
+        {
+            System.out.println("Oh no! You rolled a " + total + ". You lost.");
+        }
     }
-    
-    
 }
